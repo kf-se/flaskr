16eccfc97b18e07939c07c6ec590680592241245
kf-se <sidllub@gmail.com>
2022-05-03 20:14:42 +0200

    added an app.py

--- app.py
+++ app.py
@@ -2,12 +2,7 @@ from flaskr import create_app
 import os
 
 if __name__ == "__main__":
-    ON_HEROKU = os.environ.get('ON_HEROKU')
-
-    if ON_HEROKU:
-        port = int(os.environ.get('PORT', 5000))
-    else:
-        port = 5000
+    port = int(os.environ.get('PORT', 5000))
 
     app = create_app()
     app.run(host='0.0.0.0', port=port)
